a5e2afef121c3291e09b124fb95237f660c1b985
lgope <ilakshmangope@gmail.com>
2020-03-22 23:41:26 +0600

    database config | db model | article input in db 🛠

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const Article = require('./../models/article');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('This is Articles Page');
+router.get('/new', (req, res) => {
+  res.render('articles/new');
 });
 
-router.get('/test', (req, res) => {
-  res.send('This is Articles Page Loading');
+router.get('/:get', (req, res) => {});
+
+router.post('/', async (req, res) => {
+  const { title, description, markdown } = req.body;
+  const article = new Article({
+    title,
+    description,
+    markdown
+  });
+
+  try {
+    article = await article.save();
+    res.redirect(`/articles/${article.id}`);
+  } catch (error) {
+    res.render(`articles/new`, { article });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
 const app = express();
+
+mongoose.connect('mongodb://localhost/markdown-blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.set('view engine', 'ejs');
 
 app.use('/articles', articleRouter);
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   const articles = [
     {
       title: 'Test Article',
-      createdAt: Date.now(),
+      createdAt: new Date(),
       describtion: 'test description'
     },
     {
       title: 'Test Article 2',
-      createdAt: Date.now(),
+      createdAt: new Date(),
       describtion: 'test description 2'
     }
   ];
-  res.render('index', { articles });
+  res.render('articles/index', { articles });
 });
 
 const port = process.env.PORT | 3000;
