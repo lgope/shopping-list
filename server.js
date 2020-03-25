51063c8c55d161fbddeda2159aa10b8cfba8e231
lgope <ilakshmangope@gmail.com>
2020-03-25 23:00:17 +0600

    implemented edit a post functionality 🛠

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
 const app = express();
 
 mongoose.connect('mongodb://localhost/markdown-blog', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
-app.get('/', (req, res) => {
-  const articles = [
-    {
-      title: 'Test Article',
-      createdAt: new Date(),
-      describtion: 'test description'
-    },
-    {
-      title: 'Test Article 2',
-      createdAt: new Date(),
-      describtion: 'test description 2'
-    }
-  ];
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' });
   res.render('articles/index', { articles });
 });
 
