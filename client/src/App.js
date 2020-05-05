d8d3434afdcd544458aeb830a8e6690ea1ee818d
lgope <ilakshmangope@gmail.com>
2020-05-05 22:41:24 +0600

    setup client side | navbar and many more 🛠

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar.component';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppNavbar />
+      </div>
+    );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
   .then(() => console.log('DB connection successful!'))
   .catch((err) => console.log(chalk.redBright(err)));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () =>
   console.log(`App running on port ${chalk.greenBright(port)}...`)
 );
